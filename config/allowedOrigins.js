87104080c2cd2315ee06e1875cf520bab30e4299
DeeBest <simphiwedladla8@gmail.com>
2024-09-12 16:48:45 +0200

    added the live url in the allowed origins for admin panel

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -5,6 +5,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5174',
+  'https://mrovabizadmin.onrender.com',
 ];
 
 module.exports = allowedOrigins;
