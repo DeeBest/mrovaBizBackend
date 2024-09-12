bdd977822ce8d41ebb18bc9f2d97854dbd1c9ec9
DeeBest <simphiwedladla8@gmail.com>
2024-09-12 17:35:18 +0200

    added live frontend url to the backend allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5174',
   'https://mrovabizadmin.onrender.com',
+  'https://mrova-biz-hub.onrender.com',
 ];
 
 module.exports = allowedOrigins;
